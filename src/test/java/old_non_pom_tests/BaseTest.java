bc477eb77710e4acaf78bb1328b4bbe64f0b78ea
lenovo <p.kozubala@gmail.com>
2022-08-27 07:33:59 +0200

    add screenshot taking after test failed

--- src/test/java/old_non_pom_tests/BaseTest.java
+++ src/test/java/old_non_pom_tests/BaseTest.java
@@ -38,12 +38,8 @@ public class BaseTest {
 
     public JavascriptExecutor js;
 
-    By orderReceivedText = By.className("entry-title");
 
-    String assertionMsgIncorrectText = "Incorrect message text";
-    By errorMsgBar = By.cssSelector("ul.woocommerce-error>li");
-    String orderConfirmationTxt = "Zamówienie otrzymane";
-    String orderDetailsTxt = "Szczegóły zamówienia";
+
     String tourName = "Wyspy Zielonego Przylądka - Sal";
     String expectedFirstProductPrice = "5 399,00 zł";
     String expectedSecondProductPrice = "3 200,00 zł";
@@ -55,55 +51,14 @@ public class BaseTest {
     String today = LocalDate.now().format(DateTimeFormatter.ofPattern("dd MMMM, yyyy"));
     String orderQuantity = "× 1";
 
+
     By messageBar = By.cssSelector(".woocommerce-message");
     By orderConfirmation = By.className("woocommerce-thankyou-order-received");
 
-    By creditCardFormFrame = By.cssSelector("div#stripe-card-element iframe");
-    By productId = By.cssSelector("[data-product_id='389']");
-
-    By cardNumberFrame = By.cssSelector("div#stripe-card-element iframe");
-    By cardNumberField = By.name("cardnumber");
-
-    By cardExpDateFrame = By.cssSelector("div#stripe-exp-element iframe");
-    By cardExpFiled = By.name("exp-date");
-    By cvcFrame = By.cssSelector("div#stripe-cvc-element iframe");
-    By cvcField = By.name("cvc");
-
-    String browser = "chrome";
-
     String mainPageUrl = "https://fakestore.testelka.pl/";
 
-    @RegisterExtension
-    TestStatus status = new TestStatus();
 
-    @BeforeEach
-    public void setUp() throws MalformedURLException {
-        if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        } else if (browser.equalsIgnoreCase("remote")) {
-            ChromeOptions options = new ChromeOptions();
-            options.setCapability(CapabilityType.VERSION, "66");
-            options.setCapability(CapabilityType.PLATFORM_NAME, Platform.WINDOWS);
-            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
-        }
 
-        driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 20);
-        js = ((JavascriptExecutor) driver);
-    }
-
-    @AfterEach
-    public void tearDown(TestInfo info) throws IOException {
-        if (status.isFailed) {
-            System.out.println("Test screenshot is available at: " + takeScreenshot(info));
-        }
-        driver.quit();
-    }
 
     public void addProductToCart() {
         driver.findElement(By.name("add-to-cart")).click();
@@ -137,30 +92,6 @@ public class BaseTest {
         return driver.findElement(selector).getText();
     }
 
-    private String takeScreenshot(TestInfo info) throws IOException {
-        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        LocalDateTime timeNow = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH-mm-ss");
-        String path = "C:\\Users\\lenovo\\Desktop\\Programowanie\\Kursy\\testelka.pl\\failed_tests_screenshot\\"
-                + info.getDisplayName().replaceAll(": | ", "_") + "_" + formatter.format(timeNow)
-                + ".jpg";
-        FileUtils.copyFile(screenshot, new File(path));
-        return path;
-    }
-
-    private WebElement findElementInFrame(By frameLocator, By elementLocator) {
-        driver.switchTo().parentFrame();
-        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frameLocator));
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(elementLocator));
-    }
-
-    public void waitForOrderConfirmationPage() {
-        waitForInformationMessageBar(orderConfirmation);
-    }
-
-    public void waitUntilSectionLoad() {
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.blockUI.blockOverlay")));
-    }
 
     public boolean isElementDisplayed(By element) {
         return driver.findElements(element).size() != 0;
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,15 +1,25 @@
 package tests;
 
 import drivers.DriverFactory;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import utils.ConfigurationReader;
 import utils.TestDataReader;
+import utils.TestStatus;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public abstract class BaseTest {
@@ -19,6 +29,8 @@ public abstract class BaseTest {
     private final String testDataLocation = "src/main/java/test_data/TestData.properties";
     public final String configurationLocation = "src/configs/Configurations.properties";
 
+    @RegisterExtension
+    TestStatus status = new TestStatus();
     @BeforeAll
     public void getConfiguration(){
         configuration = new ConfigurationReader(configurationLocation);
@@ -30,7 +42,6 @@ public abstract class BaseTest {
         DriverFactory driverFactory = new DriverFactory();
         driver = driverFactory.create(configuration);
 
-
         //        WebDriverManager.chromedriver().setup();
         //        driver = new ChromeDriver();
 
@@ -39,7 +50,21 @@ public abstract class BaseTest {
     }
 
     @AfterEach
-    public void tearDown() {
+    public void tearDown(TestInfo info) throws IOException {
+        if (status.isFailed) {
+            System.out.println("Test screenshot is available at: " + takeScreenshot(info));
+        }
         driver.quit();
+    }
+
+    private String takeScreenshot(TestInfo info) throws IOException {
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        LocalDateTime timeNow = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH-mm-ss");
+        String path = "C:\\Users\\lenovo\\Desktop\\Programowanie\\Kursy\\testelka.pl\\failed_tests_screenshot\\"
+            + info.getDisplayName().replaceAll(": | ", "_") + "_" + formatter.format(timeNow)
+            + ".jpg";
+        FileUtils.copyFile(screenshot, new File(path));
+        return path;
     }
 }
